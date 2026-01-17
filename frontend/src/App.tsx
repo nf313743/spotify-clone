4470afe7acec9605100d0e6f002441648d52f3c2
nf313743 <1612339+nf313743@users.noreply.github.com>
2026-01-17 17:04:59 +0000

    80 completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { HomePage } from "./pages/home/HomePage";
 import { MainLayout } from "./layout/MainLayout";
 import { ChatPage } from "./pages/chat/ChatPage";
 import { AlbumPage } from "./pages/album/AlbumPage";
+import AdminPage from "./pages/admin/AdminPage";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -20,12 +22,15 @@ function App() {
           }
         ></Route>
         <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+
         <Route element={<MainLayout />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/chat" element={<ChatPage />} />
-          <Route  path="/albums/:albumId" element={<AlbumPage />}/>
+          <Route path="/albums/:albumId" element={<AlbumPage />} />
         </Route>
       </Routes>
+      <Toaster />
     </>
   );
 }
